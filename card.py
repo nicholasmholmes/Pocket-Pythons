7c6f5bf94c35a8a143a1c515b4688e1010d9626d
aidenhburke <aidenburke@ku.edu>
2024-10-09 17:26:57 -0500

    update card.py
    add face cards

--- card.py
+++ card.py
@@ -1,7 +1,15 @@
 class Card:
     def __init__(self, rank, suit):
-        self.rank = rank
+        self.rank = rank # 1 = ace, 11 = jack, 12 = queen, 13 = king
         self.suit = suit
 
     def __str__(self):
+        if self.rank == 1:
+            return f"Ace of {self.suit}"
+        elif self.rank == 11:
+            return f"Jack of {self.suit}"
+        elif self.rank == 12:
+            return f"Queen of {self.suit}"
+        elif self.rank == 13:
+            return f"King of {self.suit}"
         return f"{self.rank} of {self.suit}"
